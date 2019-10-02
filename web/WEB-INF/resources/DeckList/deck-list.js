d9d27ee4cee231866c4bee37c6d60a652ae2cf09
bitstudent <lmao@lmao.com>
2019-10-02 16:56:29 -0500

    Home works with Spring MVC

---
+++ web/WEB-INF/resources/DeckList/deck-list.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular.module('myApp.deckList', ['ngRoute'])
+
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/DeckList', {
+            templateUrl: 'resources/DeckList/deck-list.html',
+            controller: 'deckListCtrl'
+        });
+    }])
+
+    .controller('deckListCtrl', function($rootScope, $scope) {
+        $scope.deck = $rootScope.decklist;
+
+        $scope.removeFromDeck = function(index)
+        {
+            $rootScope.decklist.splice(index, 1);
+        }
+        $scope.setCard = function(card)
+        {
+            $rootScope.selectedCard = card;
+        }
+    });
